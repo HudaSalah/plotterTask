f6c99a28d374567b367fadef4805e3f13791c736
Huda salah <hsalah@tendegrees.sa>
2022-01-13 06:06:10 +0200

    get columns list and bind them in template

--- src/app/components/plotter/plotter.component.ts
+++ src/app/components/plotter/plotter.component.ts
@@ -1,17 +1,28 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ApiService } from 'src/app/services/api.service';
+import { Column } from 'src/app/models/Column';
 @Component({
   selector: 'app-plotter',
   templateUrl: './plotter.component.html',
-  styleUrls: ['./plotter.component.scss']
+  styleUrls: ['./plotter.component.scss'],
 })
 export class PlotterComponent implements OnInit {
+  ColumnsObj: Column[] = [];
+  constructor(private ApiService: ApiService) {}
 
-  constructor() { }
+  clearDimensionBox() {}
+  clearMeasuresBox() {}
 
-  clearDimensionBox(){}
-  clearMeasuresBox(){}
-  ngOnInit(): void {
+  getColumnsData() {
+    this.ApiService.get('https://plotter-task.herokuapp.com/columns').subscribe(
+      (res) => {
+        this.ColumnsObj = res as Column[];
+        console.log(this.ColumnsObj);
+      }
+    );
   }
 
+  ngOnInit(): void {
+    this.getColumnsData();
+  }
 }
