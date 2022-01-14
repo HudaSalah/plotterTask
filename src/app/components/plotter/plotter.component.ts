2c0c364cb66e0d947b46bd1877f96fad4316a5cc
Huda salah <hsalah@tendegrees.sa>
2022-01-14 16:46:48 +0200

    redesign plotter page and update plotter according remove dimension and measures

--- src/app/components/plotter/plotter.component.ts
+++ src/app/components/plotter/plotter.component.ts
@@ -1,13 +1,12 @@
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ApiService } from 'src/app/services/api.service';
 import { Column } from 'src/app/models/Column';
+import { PlotData } from 'src/app/models/plot-data';
 import { EChartOption } from 'echarts';
 import {
   CdkDrag,
   CdkDragDrop,
-  CdkDragEnter,
   CdkDropList,
-  moveItemInArray,
   transferArrayItem,
 } from '@angular/cdk/drag-drop';
 
@@ -18,6 +17,7 @@ import {
 })
 export class PlotterComponent implements OnInit {
   ColumnsObj: Column[] = [];
+  PlotObjs: PlotData[] = [];
   echartOptions: EChartOption;
 
   DimensionList: Array<any> = [];
@@ -27,6 +27,7 @@ export class PlotterComponent implements OnInit {
     measures: this.MeasureList,
     dimension: this.DimensionList,
   };
+  isShown: boolean = false;
   constructor(private ApiService: ApiService) {}
 
   dropListEnterPredicate(list) {
@@ -35,72 +36,68 @@ export class PlotterComponent implements OnInit {
     };
   }
 
-  dimensionDrop(event: CdkDragDrop<string[]>) {
+  onDrop(event: CdkDragDrop<string[]>, type) {
     let droppedEle =
       event.previousContainer.data[event.previousIndex]['function'];
 
-    if (
-      event.previousContainer !== event.container &&
-      droppedEle == 'dimension'
-    ) {
+    if (event.previousContainer !== event.container && droppedEle == type) {
       transferArrayItem(
         event.previousContainer.data,
         event.container.data,
         event.previousIndex,
         event.currentIndex
       );
+      this.checkFilledProperties(this.plotAxis);
     }
-    console.log(this.DimensionList);
-    this.checkProperties(this.plotAxis)
   }
 
-  checkProperties(obj) {
-    if(obj.dimension.length>0 && obj.measures.length>0){
+  checkFilledProperties(obj) {
+    if (obj.dimension.length > 0 && obj.measures.length > 0) {
       let plotAxis = {
-        measures: obj.measures.map(v => v.name),
+        measures: obj.measures.map((v) => v.name),
         dimension: obj.dimension[0].name,
       };
-      console.log(plotAxis)
-      this.getPlotData(plotAxis)
+
+      this.getPlotData(plotAxis);
+    } else {
+      // hide plot
+      this.isShown = false;
     }
   }
 
-  measuresDrop(event: CdkDragDrop<string[]>) {
-    let droppedEle =
-      event.previousContainer.data[event.previousIndex]['function'];
-
-    if (
-      event.previousContainer !== event.container &&
-      droppedEle == 'measure'
-    ) {
-      transferArrayItem(
-        event.previousContainer.data,
-        event.container.data,
-        event.previousIndex,
-        event.currentIndex
-      );
+  clearBox(list, type) {
+    let oldList = list;
+    if (type == 'dimension') {
+      this.DimensionList = [];
+      this.plotAxis.dimension = this.DimensionList;
+    } else {
+      this.MeasureList = [];
+      this.plotAxis.measures = this.MeasureList;
     }
-    console.log(this.MeasureList);
-    console.log(this.plotAxis);
-    this.checkProperties(this.plotAxis)
-  }
-
-  clearDimensionBox() {}
-  clearMeasuresBox() {}
-
-  getColumnsData() {
-    this.ApiService.get('https://plotter-task.herokuapp.com/columns').subscribe(
-      (res) => {
-        this.ColumnsObj = res as Column[];
-        console.log(this.ColumnsObj);
-      }
-    );
+    this.ColumnsObj.push(...oldList);
+    this.checkFilledProperties(this.plotAxis);
   }
 
   drawPlot(plotData) {
+    let legend = [];
+    let xAxisObj = plotData[0];
+    //get yAxis valus & legend
+    plotData.splice(0, 1);
+    plotData = plotData.map((v) => ({
+      name: v.name,
+      data: v.values,
+      type: 'line',
+      stack: 'Total',
+    }));
+
+    plotData.forEach((element) => {
+      legend.push(element.name);
+    });
+
+    this.isShown = true;
     this.echartOptions = {
       legend: {
-        data: [plotData[1].name],
+        data: legend,
       },
       grid: {
         left: '3%',
@@ -109,7 +106,7 @@ export class PlotterComponent implements OnInit {
         containLabel: true,
       },
       xAxis: {
-        name: plotData[0].name,
+        name: xAxisObj.name,
         nameLocation: 'center',
         nameTextStyle: {
           color: '#1e8fff',
@@ -118,18 +115,18 @@ export class PlotterComponent implements OnInit {
         },
         type: 'category',
         boundaryGap: true,
-        data: plotData[0].values,
+        data: xAxisObj.values,
         nameGap: 20,
       },
       yAxis: {
-        name: plotData[1].name,
-        nameLocation: 'end',
         type: 'value',
-        nameTextStyle: {
-          color: '#1e8fff',
-          fontWeight: 'bold',
-          fontSize: 14,
-        },
+        // name: plotData[1].name,
+        // nameLocation: 'end',
+        // nameTextStyle: {
+        //   color: '#1e8fff',
+        //   fontWeight: 'bold',
+        //   fontSize: 14,
+        // },
       },
       tooltip: {
         trigger: 'item',
@@ -139,14 +136,7 @@ export class PlotterComponent implements OnInit {
           return `<b>${params['name']}</b> : $ ${params['value']}`;
         },
       },
-      series: [
-        {
-          name: plotData[1].name,
-          type: 'line',
-          stack: 'Total',
-          data: plotData[1].values,
-        },
-      ],
+      series: plotData,
     };
   }
 
@@ -155,9 +145,23 @@ export class PlotterComponent implements OnInit {
       'https://plotter-task.herokuapp.com/data',
       plotAxis
     ).subscribe((res) => {
-      this.drawPlot(res);
-      console.log(res);
+      this.PlotObjs = res as PlotData[];
+      this.drawPlot(this.PlotObjs);
+    },
+    (err)=>{
+      console.log(err)
     });
+  }
+
+  getColumnsData() {
+    this.ApiService.get('https://plotter-task.herokuapp.com/columns').subscribe(
+      (res) => {
+        this.ColumnsObj = res as Column[];
+      },
+      (err)=>{
+        console.log(err)
+      }
+    );
   }
 
   ngOnInit(): void {
