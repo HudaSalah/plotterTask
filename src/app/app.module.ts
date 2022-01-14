0fffcb36d1709a54406445e23d39c0080109dec2
Huda salah <hsalah@tendegrees.sa>
2022-01-14 11:58:10 +0200

    drag column and drop to box according function type then draw plot

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PlotterComponent } from './components/plotter/plotter.component';
-
+import { DragDropModule } from '@angular/cdk/drag-drop';
 import { NgxEchartsModule } from 'ngx-echarts';
 @NgModule({
   declarations: [
@@ -15,7 +15,8 @@ import { NgxEchartsModule } from 'ngx-echarts';
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    NgxEchartsModule
+    NgxEchartsModule,
+    DragDropModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/plotter/plotter.component.ts
+++ src/app/components/plotter/plotter.component.ts
@@ -2,6 +2,14 @@ import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { ApiService } from 'src/app/services/api.service';
 import { Column } from 'src/app/models/Column';
 import { EChartOption } from 'echarts';
+import {
+  CdkDrag,
+  CdkDragDrop,
+  CdkDragEnter,
+  CdkDropList,
+  moveItemInArray,
+  transferArrayItem,
+} from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-plotter',
@@ -12,7 +20,70 @@ export class PlotterComponent implements OnInit {
   ColumnsObj: Column[] = [];
   echartOptions: EChartOption;
 
+  DimensionList: Array<any> = [];
+  MeasureList: Array<any> = [];
+
+  plotAxis = {
+    measures: this.MeasureList,
+    dimension: this.DimensionList,
+  };
   constructor(private ApiService: ApiService) {}
+
+  dropListEnterPredicate(list) {
+    return function (drag: CdkDrag, drop: CdkDropList) {
+      return list.length < 1;
+    };
+  }
+
+  dimensionDrop(event: CdkDragDrop<string[]>) {
+    let droppedEle =
+      event.previousContainer.data[event.previousIndex]['function'];
+
+    if (
+      event.previousContainer !== event.container &&
+      droppedEle == 'dimension'
+    ) {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      );
+    }
+    console.log(this.DimensionList);
+    this.checkProperties(this.plotAxis)
+  }
+
+  checkProperties(obj) {
+    if(obj.dimension.length>0 && obj.measures.length>0){
+      let plotAxis = {
+        measures: obj.measures.map(v => v.name),
+        dimension: obj.dimension[0].name,
+      };
+      console.log(plotAxis)
+      this.getPlotData(plotAxis)
+    }
+  }
+
+  measuresDrop(event: CdkDragDrop<string[]>) {
+    let droppedEle =
+      event.previousContainer.data[event.previousIndex]['function'];
+
+    if (
+      event.previousContainer !== event.container &&
+      droppedEle == 'measure'
+    ) {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      );
+    }
+    console.log(this.MeasureList);
+    console.log(this.plotAxis);
+    this.checkProperties(this.plotAxis)
+  }
 
   clearDimensionBox() {}
   clearMeasuresBox() {}
@@ -26,38 +97,38 @@ export class PlotterComponent implements OnInit {
     );
   }
 
-  drawPlot(plotData){
+  drawPlot(plotData) {
     this.echartOptions = {
       legend: {
-        data: [plotData[1].name]
+        data: [plotData[1].name],
       },
       grid: {
         left: '3%',
         right: '4%',
         bottom: '3%',
-        containLabel: true
+        containLabel: true,
       },
       xAxis: {
-        name:plotData[0].name,
-        nameLocation : 'center',
-        nameTextStyle:{
-          color:'#1e8fff',
-          fontWeight:'bold',
-          fontSize :14,
+        name: plotData[0].name,
+        nameLocation: 'center',
+        nameTextStyle: {
+          color: '#1e8fff',
+          fontWeight: 'bold',
+          fontSize: 14,
         },
         type: 'category',
         boundaryGap: true,
         data: plotData[0].values,
-        nameGap:20,
+        nameGap: 20,
       },
       yAxis: {
-        name:plotData[1].name,
-        nameLocation : 'end',
+        name: plotData[1].name,
+        nameLocation: 'end',
         type: 'value',
-        nameTextStyle:{
-          color:'#1e8fff',
-          fontWeight:'bold',
-          fontSize :14,
+        nameTextStyle: {
+          color: '#1e8fff',
+          fontWeight: 'bold',
+          fontSize: 14,
         },
       },
       tooltip: {
@@ -66,34 +137,30 @@ export class PlotterComponent implements OnInit {
         transitionDuration: 0.2,
         formatter: function (params) {
           return `<b>${params['name']}</b> : $ ${params['value']}`;
-        }
+        },
       },
-      series: [{
-        name: plotData[1].name,
-        type: 'line',
-        stack: 'Total',
-        data: plotData[1].values
-      }]
-    }
+      series: [
+        {
+          name: plotData[1].name,
+          type: 'line',
+          stack: 'Total',
+          data: plotData[1].values,
+        },
+      ],
+    };
   }
 
-
-
- getPlotData(){
-    let body = {
-      measures:['Cost'],
-      dimension: 'Product'
-    }
-   this.ApiService.post('https://plotter-task.herokuapp.com/data',body).subscribe(
-      (res) => {
-        this.drawPlot(res);
-        console.log(res);
-      }
-    );
+  getPlotData(plotAxis) {
+    this.ApiService.post(
+      'https://plotter-task.herokuapp.com/data',
+      plotAxis
+    ).subscribe((res) => {
+      this.drawPlot(res);
+      console.log(res);
+    });
   }
 
   ngOnInit(): void {
     this.getColumnsData();
-    // this.getPlotData();
   }
 }
